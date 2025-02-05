3b491fe8153ff744a54826e152e538b2346b87b6
harocorcon <hracconsular@gmail.com>
2025-02-05 14:09:48 +0800

    login buttons etc

--- app/components/JokeFetcher.jsx
+++ app/components/JokeFetcher.jsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 
-export default function JokeFetcher() {
+export default function JokeFetcher({user}) {
     const [joke, setJoke] = useState('')
 
     const fetchJoke = async () => {
@@ -19,15 +19,25 @@ export default function JokeFetcher() {
         fetchJoke();
     }, []);
 
+    const saveJokeText = user? 'Save Joke' : 'Login to save joke';
+
     return (
         <div>
             <p className="text-lg md:text-xl lg:text-2xl p-5">
                 {joke || 'Loading...'}
             </p>
-            <div >
+            <div className="flex justify-center gap-10" >
                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 rounded"
                  onClick={fetchJoke}>
                     Regenerate
+                </button>
+
+                <button className="bg-green-500 hover:bg-green-700 text-white font-bold p-2 rounded"
+                disabled={!user}
+                    onClick={async() => {
+                        if(!user) return;
+                        alert('joke saved')
+                    }}>{saveJokeText}
                 </button>
             </div>
         </div>
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,14 @@
-import JokeFecther from "./components/JokeFetcher";
+import JokeFetcher from "./components/JokeFetcher";
+import { createClient } from "./utils/supabase/server";
 
-export default function Home() {
+export default async function Home() {
+
+  const supabase = createClient();
+  const { data } = await (await supabase).auth.getUser();
+
   return (
     <main className="bg-gray-800 min-h-screen flex items-center justify-center text-center text-white">
-        <JokeFetcher />
+        <JokeFetcher user={data.user} />
     </main>
   );
 }
