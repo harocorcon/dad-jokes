1077d117e0b0f1bd6a068342c1cb071cc688ce77
harocorcon <hracconsular@gmail.com>
2025-02-06 16:00:42 +0800

    login fix

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -11,6 +11,7 @@ export default function Login(){
     return (
         <main className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
             <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <h1 className="mt-3 w-full text-2xl text-center">Dadjokez</h1>
                 <form className="p-6 space-y-4 md:space-y-6 sm:p-8">
                     <div>
                         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="email">Email</label>
@@ -44,7 +45,7 @@ export default function Login(){
                         }
                     </div>
                 </form>
-
+            <div className="flex justify-center p-3">
                     {
                         !isSigningUp ? (
                             <p className="items-center justify-center text-sm font-light text-gray-500 dark:text-gray-400"> Don't have an account? {' '}
@@ -57,12 +58,13 @@ export default function Login(){
                         ) : (
                             <p  className="items-center justify-center text-sm font-light text-gray-500 dark:text-gray-400"> Already have an account? {' '}
                                 <button className="font-medium text-blue-600 hover:underline dark:text-primary-500"
-                                type="submit" formAction={login}>
+                                type="submit" formAction={login} onClick={() => setIsSigningUp(false)}>
                                     Login
                                 </button>
                             </p>
                         )
                     }
+                </div>
             </div>
         </main>
     )
